8240ff14857080a43626332843e765156c3590d9
Astrocat2374 <sugiyama.evan@gmail.com>
2017-11-22 14:40:18 -1000

    Added base code

---
+++ app.js
@@ -0,0 +1,36 @@
+var blue = Math.floor((Math.random() * 3000 ) + 1000 );
+var green = Math.floor((Math.random() * 6000 ) + 3000 );
+var red = Math.floor((Math.random() * 3000 ) + 1000 );
+
+console.log(blue)
+console.log(green)
+console.log(red)
+
+aliens(blue,green,red);
+
+function aliens() {
+	
+	if( blue + green + red = 10000 && green > 5000 ) {
+			
+		console.log("MOTHER OF GOD, not the GREEN ones");
+		
+	} else {
+
+	}
+
+	if( blue + green + red = 10000 && green + red > 8000 ) {
+			
+		console.log("DOOOOOM");
+
+	}  else {
+
+	}
+
+	if( red + blue < 3000 ) {
+		
+		console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+
+	} else {
+
+	}
+}
